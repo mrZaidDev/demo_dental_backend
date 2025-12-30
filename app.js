82379f84c8352dbf19abeefbc70dbf65e9ed856a
Zaid Khan <zaidcodingtopcoding@gmail.com>
2025-12-30 08:42:39 +0500

    done

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors(
   {
-    origin: 'http://localhost:5173',
+    origin: 'https://demo-dental-frontend.vercel.app',
     credentials: true
   }
 ));
